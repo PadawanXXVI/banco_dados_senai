955998835f0517c8a06d33863f5039d4f1beadb2
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2024-12-05 17:30:32 -0300

    Finalização da aula 32 e das atividades

--- aula32/aula32.sql
+++ aula32/aula32.sql
@@ -79,6 +79,20 @@ SELECT soma(15, 17) AS Adição;
 
 -- EXEMPLO 05: verifica se o número é positivo ou negativo
 DELIMITER $$
+CREATE FUNCTION verificar (num INT)
+RETURNS VARCHAR(15) DETERMINISTIC
+BEGIN
+	RETURN CASE
+		WHEN num >= 0 THEN 'Positivo'
+        ELSE 'Negativo'
+	END;
+END $$
+DELIMITER ;
+SELECT verificar(-5);
+
+-- EXEMPLO 06: Verficiar se um número é par ou ímpar
+
+DELIMITER $$
 CREATE FUNCTION paridade (numero INT)
 RETURNS TEXT DETERMINISTIC
 BEGIN
@@ -106,4 +120,90 @@ BEGIN
 END $$
 DELIMITER ;
 SELECT paridade2(13) AS Paridade;
+
+-- VISUALIZAR AS FUNÇÕES CRIADAS
+SHOW FUNCTION STATUS WHERE db = 'db_funcao'; -- visualiza a partir de um banco de dados específico
+SHOW FUNCTION STATUS; -- visualiza a partir do banco de dados selecionado
+
+-- APAGAR UMA FUNÇÃO
+DROP FUNCTION nome_function;
+
+-- MOSTRAR A ESTRUTURA DE UMA FUNÇÃO
+SHOW CREATE FUNCTION soma;
+
+###################### ATIVIDADES #####################
+-- AT01: CRIE UMA FUNÇÃO QUE RECEBA UMA IDADE E RETORNE SE É MAIOR OU MENOR DE IDADE
+DELIMITER $$
+CREATE FUNCTION maioridade(idade INT)
+RETURNS TEXT DETERMINISTIC
+BEGIN
+	RETURN CASE
+		WHEN idade >= 18 THEN 'Maior de idade'
+        ELSE 'Menor de idade'
+	END;
+END $$
+DELIMITER ;
+
+SELECT maioridade(25) AS `Condição penal`;
+
+-- AT02: CRIE UMA FUNÇÃO QUE RECEBA DOIS NÚMEROS E RETORNE O MAIOR DELES
+DELIMITER $$
+CREATE FUNCTION maior (numero1 INT, numero2 INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+	RETURN CASE
+		WHEN numero1 > numero2 THEN numero1
+        ELSE numero2
+	END;
+END $$
+DELIMITER ;
+
+SELECT maior(-5,-4);
+
+-- AT03: CRIE UMA FUNÇÃO QUE CONVERTA UMA STRING PARA MAIÚSCULA
+DELIMITER $$
+CREATE FUNCTION maiusculo(enunciado TEXT)
+RETURNS TEXT DETERMINISTIC
+BEGIN
+	RETURN upper(enunciado);
+END $$
+DELIMITER ;
+
+SELECT maiusculo('a vida é bela');
+
+-- AT04: CRIE UMA FUNÇÃO PARA RETORNAR O QUADRADO DE UM NÚMERO
+DELIMITER $$
+CREATE FUNCTION quadrado(numero INT)
+RETURNS INT DETERMINISTIC
+BEGIN
+	RETURN power(numero,2);
+END $$
+DELIMITER ;
+
+SELECT quadrado(25);
+
+-- AT05: CRIE UMA FUNÇÃO PARA CONTAR A QUANTIDADE DE CARACTERES DE UMA STRING
+DELIMITER $$
+CREATE FUNCTION tamanho (enunciado TEXT)
+RETURNS INT DETERMINISTIC
+BEGIN
+	RETURN length(enunciado);
+END $$
+DELIMITER ;
+
+SELECT tamanho ('Anderson');
+
+-- AT06: CRIE UMA FUNÇÃO QUE SOME DOIS NÚMEROS E RETRONE SE ELE É POSITIVO OU NEGATIVO
+DELIMITER $$
+CREATE FUNCTION par (a INT, b INT)
+RETURNS TEXT DETERMINISTIC
+BEGIN
+	DECLARE soma INT;
+    SET soma = a + b;
+    RETURN CONCAT('O resultado é ', soma, ' e ', IF (soma >= 0, 'a soma é positiva', 'a soma é negativa'));
+END $$
+DELIMITER ;
+
+SELECT par(-20,4) AS Resultado;
+    
     
