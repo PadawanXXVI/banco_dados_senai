4cb82ad0e6148d1c41f8373ef54580bb16ea0592
Anderson de Matos Guimarães <anderson.m.guimaraes@icloud.com>
2024-12-09 08:34:44 -0300

    Criação do arquivo da segunda revisão de SQL

---
+++ revisoes/revisao2.sql
@@ -0,0 +1,12 @@
+/*
+REVISÃO PARA SEGUNDA PROVA DE SQL
+Conteúdo:
+    Relacionamento entre tabelas;
+    INNER JOIN;
+    Funções de data;
+    VIEW;
+    CASE... WHEN;
+    FUNCTION;
+    STORE PROCEDURE
+*/
+
